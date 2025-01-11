15e6f958eeb3b8865adf7ef71a33d2fffc2ebfa3
Ananya Kini <ananyakini105@gmail.com>
2025-01-11 11:32:01 +0530

    Add alerting system

---
+++ alertingsystem.py
@@ -0,0 +1,21 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    'critical_logs',
+    'node_failure',
+    bootstrap_servers=['localhost:9092'],
+    value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+)
+
+for message in consumer:
+    data = message.value 
+    log_type = data.get('message_type', 'unknown')  
+    status=data.get('status','unknown')
+    if log_type == 'REGISTRATION' and status =='UP':
+        print("🌟 Registration Successful: A new node has been registered.")
+    elif log_type =='REGISTRATION' and status =='DOWN':
+        print("⚠️ Alert: A node's heartbeat has stopped.")
+    else:
+        print("❌ Error Alert: An error occurred in your application!")
+
